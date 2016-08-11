c358bfa581c46572b5900df0fa275fbb9c075efb
jon-is-learning <jonathan.garrett@gmail.com>
2016-08-10 19:20:47 -0700

    implement tutorial solutions

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,8 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    this.set('like', !this.get('like'));
+    this.collection.sort();
   }
 
 });
@@ -16,12 +17,14 @@ var Movies = Backbone.Collection.extend({
 
   initialize: function() {
     // your code here
+    
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field;
+    this.sort();
   }
 
 });
@@ -59,6 +62,7 @@ var MovieView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    this.model.on('change:like', this.render, this);
   },
 
   events: {
@@ -67,6 +71,7 @@ var MovieView = Backbone.View.extend({
 
   handleClick: function() {
     // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -80,6 +85,7 @@ var MoviesView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    this.collection.on('sort', this.render, this);
   },
 
   render: function() {
